5db9ad6df654fccbac50591d0ed154feb3df9cb8
Krish Raj <raj.krish.1819@gmail.com>
2026-08-17 23:28:08 +0530

    Time: 3 ms (81.12%), Space: 10.2 MB (27.84%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -6,22 +6,33 @@ public:
             return "1";
         }
         string str = countAndSay(n-1);
-        int freq = 1;
         string ztr = "";
-        char ch = str[0];
-        char dh;
-        for(int i = 1; i < str.length(); i++){
-            dh = str[i];
-            if(ch == dh){
-                freq++;
+        // int freq = 1;
+        // char ch = str[0];
+        // char dh;
+        // for(int i = 1; i < str.length(); i++){
+        //     dh = str[i];
+        //     if(ch == dh){
+        //         freq++;
+        //     }
+        //     else{
+        //         ztr = ztr + to_string(freq) + ch;
+        //         freq = 1;
+        //         ch = dh;
+        //     }
+        // }
+        // ztr += to_string(freq) + ch; // bcoz loop doesn't add last freq and its char
+        int cnt = 0;
+        int i = 0;
+        while(i < str.length()){
+            char ch = str[i];
+            while(i < str.length() && str[i] == ch){
+                cnt++;
+                i++;
             }
-            else{
-                ztr = ztr + to_string(freq) + ch;
-                freq = 1;
-                ch = dh;
-            }
+            ztr += to_string(cnt) + ch;
+            cnt = 0;
         }
-        ztr += to_string(freq) + ch; // bcoz loop doesn't add last freq and its char
         return ztr;
     }
 };
